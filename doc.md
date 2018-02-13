fdd7124b44b8d47a7b3d0972e37b6d335da31ccf
jiangwenhua <jiangwenhua@lechebang.com>
2018-02-13 16:11:42 +0800

    testcase

--- doc.md
+++ doc.md
@@ -1,8 +1,12 @@
 ### 1.AbstractRoutingDataSource动态切换数据源，实现主从读写分离
+```
 在Spring 2.0.1中引入了AbstractRoutingDataSource, 该类充当了DataSource的路由中介, 能有在运行时, 根据某种key值来动态切换到真正的DataSource上。
 具体的实现就是，虚拟的DataSource仅需继承AbstractRoutingDataSource实现determineCurrentLookupKey（）在其中封装数据源的选择逻辑
 再利用AOP拦截jpa dao接口，方法名以find开头的均使用从库查询，其余使用主库
+```
 ### 2.Docker 模拟多库，开启binlog,实现主从复制
 ### 3.Spring cache
+```
 自定义cacheManager和cache实现，增加自定义过期时间
 启用：<cache:annotation-driven cache-manager="myCacheManager"/>
+```
--- test/ConcurrentTest.java
+++ test/ConcurrentTest.java
@@ -1,54 +1,31 @@
+import com.alibaba.fastjson.JSONObject;
+import com.jwh.demo.CommonRequest;
 import com.jwh.demo.StartServer;
+import com.jwh.demo.controller.UserController;
+import com.jwh.demo.model.User;
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.data.redis.core.RedisTemplate;
 
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.List;
 
 
 public class ConcurrentTest {
 
-    private static Lock lock = new ReentrantLock();
+    private UserController userController;
 
-    private RedisTemplate<String,String> redisTemplate;
-
-    static class Outer{
-        public  void print(String str){
-//        lock.lock();
-//            synchronized (this){
-                for(int i=0;i<str.length();i++){
-                    System.out.print(str.charAt(i));
-                    try{
-                        TimeUnit.MILLISECONDS.sleep(100);
-                    }catch (Exception e){
-                        e.printStackTrace();
-                    }
-                }
-                System.out.println();
-            }
-//        lock.unlock();
-//        }
-    }
-
-
-//    public static void main(String[] args){
-//        String str1 = "abcdefghijklmn";
-//        String str2 = "opqrstuvwxyz";
-//        Outer outer = new Outer();
-//        new Thread(()-> outer.print(str1)).start();
-//        new Thread(()-> outer.print(str2)).start();
-//    }
     @Before
     public void startUp(){
         StartServer.main(new String[]{});
-        redisTemplate = StartServer.context.getBean("stringRedisTemplate",RedisTemplate.class);
+        userController = StartServer.context.getBean("userController",UserController.class);
     }
 
     @Test
-    public void test(){
-
+    public void test() throws Exception{
+        JSONObject json = new JSONObject();
+        json.put("id",12);
+        CommonRequest request = new CommonRequest(json);
+        List<User> users =  userController.get(request);
+        users.forEach(user -> System.out.println(user.getUserName()));
     }
 
 }
